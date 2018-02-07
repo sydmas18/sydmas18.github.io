89d559a3aa3de0eabe449373f2c52d8ef13d4f3e
sydmas18 <36240371+sydmas18@users.noreply.github.com>
2018-02-07 18:59:59 +0100

    index.html upload

---
+++ home.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Home</title>
+	<link rel="stylesheet" type="text/css" href="home.css">
+</head>
+<body>
+	<header>
+		<div class="nav-bar">
+			<img src="logo.png" class="logo">
+
+		<ul class="menu">
+			<li><a href="#">Home</a></li>
+			<li><a href="#">Over mij</a></li>
+			<li><a href="#">Diensten</a></li>
+			<li><a href="#">Portfolio</a></li>
+			<li><a href="#">Contact</a></li>
+		</ul>
+	</div>
+
+	<div class="welcome">
+		<h1>Welkom op mijn website!</h1>
+		<a href="#" class="btn btn1">Over mij</a>
+	</div>
+
+	</header>
+</body>
+</html>
